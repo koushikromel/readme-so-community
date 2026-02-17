2771394643390586db23ef83adc47e141b7d4409
Koushik Romel <koushikromel@gmail.com>
2026-02-17 11:04:14 +0530

    feat: integrate Vercel Analytics for page view tracking

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,15 @@ import { appWithTranslation } from 'next-i18next'
 import SEO from '../next-seo.config.js'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 
+import { Analytics } from '@vercel/analytics/next'
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <ProgressBar height="4px" color="#10b981" options={{ showSpinner: false }} shallowRouting />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
